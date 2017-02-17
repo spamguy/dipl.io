9defb7fd76f6081a293e35511b1128fe36f1370b
Will Oram <spamguy@spamguy.org>
2017-02-17 16:43:40 -0600

    Dropping dividers from list items because they're not good UI policy. Added Compose New Press section with chips

--- client/app/components/gametools/gametools.controller.js
+++ client/app/components/gametools/gametools.controller.js
@@ -2,9 +2,14 @@ angular.module('gametools.component')
 .controller('GameToolsController', ['gameService', '$scope', function(gameService, $scope) {
     var vm = this;
 
+    vm.selectedItem = null;
+    vm.nationSearchText = null;
+    vm.selectedNations = [];
+
     vm.gameService = gameService;
     vm.setPhaseState = setPhaseState;
     vm.refreshPress = refreshPress;
+    vm.searchNations = searchNations;
 
     function setPhaseState() {
         gameService.setPhaseState(vm.service.game, vm.service.getCurrentPhase(), vm.service.phaseState);
@@ -15,5 +20,17 @@ angular.module('gametools.component')
         .then(function(channels) {
             vm.channels = channels;
         });
+    }
+
+    function searchNations(query) {
+        var n = 0,
+            results = [];
+
+        for (; n < vm.service.variant.Nations.length; n++) {
+            if (_.startsWith(vm.service.variant.Nations[n].toLowerCase(), query.toLowerCase()))
+                results.push(vm.service.variant.Nations[n]);
+        }
+
+        return results;
     }
 }]);
--- client/app/components/presschannel/presschannel.controller.js
+++ client/app/components/presschannel/presschannel.controller.js
@@ -1,4 +1,3 @@
 angular.module('gametools.component')
 .controller('PressChannelController', [function() {
-    var vm = this;
 }]);
