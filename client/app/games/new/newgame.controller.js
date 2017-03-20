612d7f2ea4ca10d00e204c6d55751c92cc3e573a
Will Oram <spamguy@spamguy.org>
2017-03-20 16:30:30 -0500

    Removed obsolete new game options. Added min/max rating options

--- client/app/games/new/newgame.controller.js
+++ client/app/games/new/newgame.controller.js
@@ -5,17 +5,17 @@ angular.module('games')
     var vm = this;
 
     angular.extend(vm, {
+        restrictions: {
+            minimum: {
+                enabled: false,
+                rating: 1000
+            },
+            maximum: {
+                enabled: false,
+                rating: 2000
+            }
+        },
         move: {
-            minutes: 0,
-            hours: 0,
-            days: 1
-        },
-        retreat: {
-            minutes: 0,
-            hours: 0,
-            days: 1
-        },
-        adjust: {
             minutes: 0,
             hours: 0,
             days: 1
@@ -27,14 +27,25 @@ angular.module('games')
             Desc: '(no title)',
             Variant: 'Classical',
             PhaseLengthMinutes: 60 * 24,
+            MinRating: null,
+            MaxRating: null,
 
             save: function() {
                 // Calculate minutes in supplied deadline.
                 vm.game.PhaseLengthMinutes = vm.move.minutes + (vm.move.hours * 60) + (vm.move.days * 24 * 60);
 
+                // Apply restrictions as desired.
+                if (vm.restrictions.minimum.enabled)
+                    vm.game.MinRating = vm.restrictions.minimum.rating;
+                if (vm.restrictions.maximum.enabled)
+                    vm.game.MaxRating = vm.restrictions.maximum.rating;
+
                 return gameService.createNewGame(vm.game)
                 .then(function(game) {
                     return $state.go('profile.games');
+                })
+                .catch(function(ex) {
+                    console.error(ex);
                 });
             }
         }
